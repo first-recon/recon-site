ecbd40abd0c90bed523b02d10e90529c60e990e1
Jeremy School <jschool@catalyte.io>
2018-06-05 12:04:14 -0400

    things

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import Container from '../lib/components/container/container.js';
 
 import Landing from './landing/landing.js';
+import TeamDetail from './team-detail';
 
 export default {
 	landing: {
 		url: '',
 		component: function () { return Container(Landing()) }
+	},
+	team: {
+		url: /(team)\/\d+/,
+		component: function () { return Container(TeamDetail()) }
 	}
 }
